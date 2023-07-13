6c9bc40d60fd3c345434cf7e52ab7af4244b86a2
bspsripathi <132542049+bspsripathi@users.noreply.github.com>
2023-07-13 09:06:46 +0530

    Add files via upload

---
+++ src/app/employee.model.ts
@@ -0,0 +1,11 @@
+export class Employee {
+        id?: number;
+        employee_email?:string;
+        First_name?: string;
+        Last_name?: string;
+    }
+    export class admin {
+        id=1;
+        email?: string;
+        password?: string;
+    }
